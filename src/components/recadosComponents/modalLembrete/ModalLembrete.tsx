a512ccd5fb4b5bd6381250a22896e3edd2ad8ca9
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-02-20 22:56:48 -0300

    atualizacoes na modalLembrete

--- src/components/recadosComponents/modalLembrete/ModalLembrete.tsx
+++ src/components/recadosComponents/modalLembrete/ModalLembrete.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import CancelIcon from '@mui/icons-material/Cancel';
@@ -23,48 +23,62 @@ function ModalLembrete(lembreteInfo : ReminderInfos) {
             });
         }
     }, [showReminderModalState])
-    function reminderHandle() {
-        if (showReminderModalState.type === "new") {
-            dispatch(newReminder({
-                accountID: lembreteInfo.accountId,
-                acao: '',
-                data: '',
-                hora: '',
-                descricao: '',
+    const [preLoadInputs, setPreLoadInputs] = useState(0);
+    const [inputAcao, setInputAcao] = useState("");    
+    const [inputData, setInputData] = useState("");
+    const [inputHora, setInputHora] = useState("");
+    const [inputDescricao, setInputDescricao] = useState("");
+    const formModalLembrete : any = useRef();
+    useEffect(() => {
+        if (document.readyState === "complete") {
+            formModalLembrete.current.addEventListener("submit", ((event : any) => {
+                event.preventDefault();
+                if (showReminderModalState.type === "new") {
+                    dispatch(newReminder({
+                        accountID: lembreteInfo.accountId,
+                        acao: inputAcao,
+                        data: inputData,
+                        hora: inputHora,
+                        descricao: inputDescricao,
+                    }));
+                }
+                else if (showReminderModalState.type === "edit") {
+        
+                };
             }));
         }
-        else if (showReminderModalState.type === "edit") {
-
-        };
-    }
+        else {
+            setPreLoadInputs(preLoadInputs + 1)
+        }
+    }, [preLoadInputs]);
     return(
-        <FormularioLembrete sx={modalVisual}>
+        <FormularioLembrete ref={formModalLembrete} sx={modalVisual}>
             <TabelaLembrete>
                 <tbody>
                     <tr>
                         <ThAcao>
                             <LabelInput htmlFor="input-acao">Ação:&nbsp;</LabelInput>
-                            <InputAcao type="text" required autoComplete="off" id="input-acao" maxLength={21} />
+                            <InputAcao onChange={((event) => { setInputAcao(event.target.value) })} type="text" required autoComplete="off" id="input-acao" maxLength={21} />
                         </ThAcao>
                         <ThData>
                             <LabelInput htmlFor="input-data">Data:&nbsp;</LabelInput>
-                            <InputData type="date" required autoComplete="off" id="input-data" maxLength={8} max="2100-11-23" />
+                            <InputData onChange={((event) => { setInputData(event.target.value) })} type="date" required autoComplete="off" id="input-data" maxLength={8} max="2100-11-23" />
                         </ThData>
                         <ThHora>
                             <LabelInput htmlFor="input-hora">Hora:&nbsp;</LabelInput>
-                            <InputHora type="time" required autoComplete="off" id="input-hora" />
+                            <InputHora onChange={((event) => { setInputHora(event.target.value) })} type="time" required autoComplete="off" id="input-hora" />
                         </ThHora>
                     </tr>
                     <tr>
                         <ThDescricao colSpan={4} sx={{boxShadow: '4px 3.5px 3.5px #000000;'}}>
                             <LabelInput htmlFor="input-descricao" sx={{ marginBottom: '5px' }}>Descrição:&nbsp;</LabelInput>
-                            <InputDescricao maxLength={66} id="input-descricao" required autoComplete="off" />
+                            <InputDescricao onChange={((event) => { setInputDescricao(event.target.value) })} maxLength={66} id="input-descricao" required autoComplete="off" />
                         </ThDescricao>
                     </tr>
                 </tbody>
             </TabelaLembrete>
             <DivBotoes>
-                <BotaoConfirmar onClick={(() => { reminderHandle() })} variant="contained" size="large" startIcon={<CheckCircleIcon />}>
+                <BotaoConfirmar type="submit" variant="contained" size="large" startIcon={<CheckCircleIcon />}>
                     Confirmar
                 </BotaoConfirmar>
                 <BotaoCancelar onClick={(() => { dispatch(hideReminderModal()); setModalVisual(modalVisualProps); })} variant="contained" size="large" startIcon={<CancelIcon />}>
