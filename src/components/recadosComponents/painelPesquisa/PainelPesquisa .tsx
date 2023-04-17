3bc6a661208118468440aeb071d3a634299e03c0
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-04-17 11:20:08 -0300

    pesquisa automatica ao digitar (inteligente)

--- src/components/recadosComponents/painelPesquisa/PainelPesquisa .tsx
+++ src/components/recadosComponents/painelPesquisa/PainelPesquisa .tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import SearchIcon from '@mui/icons-material/Search';
 import { Barra, BarraPesquisa, BotaoPesquisa, CheckBoxPesquisa, CheckBoxPesquisaMobile, InputPesquisa } from './painelPesquisaStyled';
@@ -10,20 +10,35 @@ import { setNeedUpdate } from "../../../redux/slices/remindersSlice";
 
 function PainelPesquisa() {
     const dispatch : UserStore = useDispatch();
-    const [checkBox, setCheckBox] = useState(false);
+    const [checkBox, setCheckBox] = useState<boolean | undefined>(false);
     const [searchValue, setSearchValue] = useState("");
+    const [searchChange, setSearchChange] = useState(0);
+    const [arrayTimers, setArrayTimers] = useState<any>([]);
     function searchSubmit() {
         const searchInfos : SearchStorageType = {
             search : searchValue,
-            checkBox : checkBox
+            checkBox : checkBox === undefined ? false : checkBox
         };
         dispatch(setSearch(searchInfos));
         dispatch(setNeedUpdate());
     };
+    
+    useEffect(() => {
+        if (searchChange > 0 || checkBox !== undefined) {
+            arrayTimers.push(setTimeout(() => {
+                searchSubmit();
+            }, 1200));
+            if(arrayTimers.length === 2) {
+                clearTimeout(arrayTimers[0]);
+                arrayTimers.splice(0, 1);
+            };  
+        };
+    }, [searchChange, checkBox]);
+
     return (
         <Barra>
             <BarraPesquisa>
-                <InputPesquisa placeholder={"Pesquisar..."} maxLength={14} onChange={((event) => {setSearchValue(event.target.value)})} />
+                <InputPesquisa placeholder={"Pesquisar..."} maxLength={14} onChange={((event) => {setSearchValue(event.target.value); setSearchChange(searchChange + 1)})} />
                 <BotaoPesquisa onClick={((event) => { event.preventDefault(); searchSubmit()})}>
                     <SearchIcon />
                 </BotaoPesquisa>
